33a87d7d847b7689d69a34331c378aec5d7b5709
BCA43 <banikmoumita99@gmail.com>
2025-09-17 12:21:22 +0530

    unique elements from list to new list

--- p1.py
+++ p1.py
@@ -1,2 +1,10 @@
-print("hello world")
-print("hello")
+# print("hello world")
+# print("hello")
+
+list1 = [1, 2, 2, 3, 4, 3, 5, 1]
+# new_list = list(set(list1))
+new_list = []
+for l in list1:
+    if l not in new_list:
+        new_list.append(l)
+print(new_list)
